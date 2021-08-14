1ceb6d49c0ce6f27efead35655a4d21c1fbd7500
Carlos Navotka <knavotka@hotmail.com>
2021-08-14 13:59:27 +0000

    Fix brackets in app py file in Flask line that were causing issues

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -28,4 +26,5 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
 
